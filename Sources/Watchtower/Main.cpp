58213c63cb3baff254435944b73065789f6fb614
Rob Yates <killowatt2213@gmail.com>
2015-06-02 17:39:47 -0400

    Reformatted project folder structure.

---
+++ Sources/Watchtower/Main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "HELLO IM COMMON LIB" << std::endl;
+	std::getchar();
+}
---
+++ Sources/WatchtowerClient/Main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "HELLO IM CLIENT" << std::endl;
+	std::getchar();
+}
